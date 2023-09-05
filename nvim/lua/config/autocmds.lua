5a4d8728aabc1a2152b5b51ff6049b6e47bf3e61
xelliott <xuananqing@gmail.com>
2023-09-05 16:24:43 +0000

    Add cuda language support to nvim-treesitter.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,8 +2,15 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp" },
+  pattern = { "c", "cpp", "cuda" },
   command = "setlocal ts=2 sw=2 et",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "cuda" },
+  callback = function(ev)
+      vim.bo[ev.buf].commentstring = "// %s"
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "c", "cpp" })
+        vim.list_extend(opts.ensure_installed, { "cuda" })
         vim.list_extend(opts.ensure_installed, { "fortran" })
         vim.list_extend(opts.ensure_installed, { "julia" })
         vim.list_extend(opts.ensure_installed, { "toml" })
