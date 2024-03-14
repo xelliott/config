af782ace5ebb6e76e2dadb7417b1ca885ea23b2b
xelliott <xuananqing@gmail.com>
2024-03-13 21:38:59 -0500

    update LazyVim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "clang-format" })
+      vim.list_extend(opts.ensure_installed, { "clang-format", "black" })
     end,
   },
   {
@@ -13,6 +13,7 @@ return {
         c = { "clang_format" },
         cpp = { "clang_format" },
         cuda = { "clang_format" },
+        python = { "black" }
       },
     },
   },
