fd646e6206443689f9dead7bfb59b6867d62cd2a
xelliott <xuananqing@gmail.com>
2024-05-29 12:39:33 -0500

    Change LS_COLORS theme.

--- generate_lscolors.sh
+++ generate_lscolors.sh
@@ -2,5 +2,6 @@
 #
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
 ls_colors_output=$(vivid -d $CDIR/filetypes.yml generate catppuccin-latte)
+ls_colors_output=$(vivid -d $CDIR/filetypes.yml generate ./gruvbox-material-light-hard.yml)
 echo "LS_COLORS=\"$ls_colors_output\"" > ${CDIR}/lscolors.sh
 echo "export LS_COLORS" >> ${CDIR}/lscolors.sh
