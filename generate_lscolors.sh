6c22786be7735700408ad6028b02ccf331516121
xelliott <xuananqing@gmail.com>
2024-06-17 23:48:59 -0500

    Revert "change lscolors"
    This reverts commit 13f47d87127778c426d77548676c40799b35a713.

---
+++ generate_lscolors.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+ls_colors_output=$(vivid -d $CDIR/filetypes.yml generate catppuccin-latte)
+ls_colors_output=$(vivid -d $CDIR/filetypes.yml generate ./gruvbox-material-light-hard.yml)
+echo "LS_COLORS=\"$ls_colors_output\"" > ${CDIR}/lscolors.sh
+echo "export LS_COLORS" >> ${CDIR}/lscolors.sh
