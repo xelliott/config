d08dcb24a26520ab4fbf09c252693e58fb71802d
xelliott <xuananqing@gmail.com>
2024-08-17 08:25:29 -0500

    add path substituions for wsl windows path

--- tools/prompt_path.nim
+++ tools/prompt_path.nim
@@ -9,6 +9,11 @@ proc substitute_path_env(path: string, env_var: string, replaced_with: string): 
         return path
     return replaced_with & path[len(env_path) .. ^1]
 
+proc substitute_path(path: string, s: string, replaced_with: string): string =
+    if s == "" or not path.startswith(s):
+        return path
+    return replaced_with & path[len(s) .. ^1]
+
 func shorten_path(path: string, max_length: int): string =
     if path.len <= max_length:
         return path
@@ -23,14 +28,24 @@ proc prompt_path(): string =
     if pwd == "":
         return r"\w"
 
-    let subsititutions = [
+    let env_subsititutions = [
         ("HOME", "~"),
         ("WORKDIR", "$WORKDIR"),
         ("WORKDIR2", "$WORKDIR2")
     ]
     var pwd_substituted = pwd
-    for (env_var, substitute) in subsititutions:
+    for (env_var, substitute) in env_subsititutions:
         pwd_substituted = substitute_path_env(pwd_substituted, env_var, substitute)
+
+    let wslenv = getEnv("WSLENV")
+    if wslenv != "":
+        let win_subsititutions = [
+            ("/mnt/c", "C:"),
+            ("/mnt/d", "D:"),
+            ("/mnt/e", "E:")
+        ]
+        for (s, substitute) in win_subsititutions:
+            pwd_substituted = substitute_path(pwd_substituted, s, substitute)
 
     return shorten_path(pwd_substituted, 12)
 
