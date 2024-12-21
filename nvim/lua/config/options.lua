e3d50881e4b6cb4ce2dd7c59399671f092173ba8
xelliott <xuananqing@gmail.com>
2024-12-21 14:08:14 -0600

    update lazyvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.snacks_animate = false
 vim.opt.showtabline = 2  -- always show the tab line
 vim.opt.autowrite = false
 vim.opt.swapfile = false
